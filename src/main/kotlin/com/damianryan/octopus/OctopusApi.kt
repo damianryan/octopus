f34bacf4a0b75e084a77461483ae4661a828969a
Damian Ryan <github@damianryan.com>
2026-07-27 16:30:20 +0100

    Switched to RestClient. (#41)
    * Switched to RestClient.
    
    * More WIP.
    
    ---------
    
    Co-authored-by: damianryan <damian@damianryan.com>

---
+++ src/main/kotlin/com/damianryan/octopus/OctopusApi.kt
@@ -0,0 +1,78 @@
+package com.damianryan.octopus
+
+import com.damianryan.octopus.model.Account
+import com.damianryan.octopus.model.Agreement
+import com.damianryan.octopus.model.Consumption
+import com.damianryan.octopus.model.ElectricityMeterPoint
+import com.damianryan.octopus.model.GasMeterPoint
+import com.damianryan.octopus.model.GridSupplyPoint
+import com.damianryan.octopus.model.GridSupplyPoints
+import com.damianryan.octopus.model.Product
+import com.damianryan.octopus.model.Products
+import com.damianryan.octopus.model.Reading
+import com.damianryan.octopus.model.Tariff
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+/**
+ * Octopus REST API.
+ *
+ * @property restClient Octopus REST API client
+ * @property properties Octopus REST API properties
+ */
+@Service
+class OctopusApi(private val restClient: OctopusRestClient, private val properties: OctopusProperties) {
+    init {
+        LoggerFactory.getLogger(OctopusApi::class.java).info("Account number: ${properties.accountNumber}")
+    }
+
+    val account: Account by lazy { restClient.get(properties.accountsUrl, Account::class.java) }
+
+    val electricityMeterPoint: ElectricityMeterPoint by lazy {
+        account.properties?.firstOrNull()?.electricityMeterPoints?.firstOrNull()!!
+    }
+
+    val mpan: String by lazy { electricityMeterPoint.mpan!! }
+
+    val electricityMeterSerialNumber: String by lazy { electricityMeterPoint.meters?.firstOrNull()?.serialNumber!! }
+
+    val electricityConsumption: List<Reading?> by lazy {
+        // https://api.octopus.energy/v1/electricity-meter-points/{mpan}/meters/{serial_number}/consumption/
+        restClient.getMany(
+            "/electricity-meter-points/${mpan}/meters/${electricityMeterSerialNumber}/consumption",
+            Consumption::class.java)
+    }
+
+    val electricityAgreements: List<Agreement> by lazy { electricityMeterPoint.agreements!!.sorted() }
+
+    val gasMeterPoint: GasMeterPoint by lazy { account.properties?.firstOrNull()?.gasMeterPoints?.firstOrNull()!! }
+
+    val mprn: String by lazy { gasMeterPoint.mprn!! }
+
+    val gasMeterSerialNumber: String by lazy { gasMeterPoint.meters?.firstOrNull()?.serialNumber!! }
+
+    val gasConsumption: List<Reading?> by lazy {
+        restClient.getMany(
+            "/gas-meter-points/${mprn}/meters/${gasMeterSerialNumber}/consumption", Consumption::class.java)
+    }
+
+    val gasAgreements: List<Agreement> by lazy { gasMeterPoint.agreements!!.sorted() }
+
+    val electricityProduct: Product by lazy {
+        restClient.get("/products/${properties.electricityProductCode}", Product::class.java)
+    }
+
+    val electricityTariff: Tariff by lazy {
+        electricityProduct.singleRegisterElectricityTariffs?.get(properties.electricityRegion)?.get(DIRECT_DEBIT)!!
+    }
+
+    val products: List<Product?> by lazy { restClient.getMany("/products", Products::class.java) }
+
+    val gridSupplyPoints: List<GridSupplyPoint?> by lazy {
+        restClient.getMany("/industry/grid-supply-points", GridSupplyPoints::class.java)
+    }
+
+    companion object {
+        const val DIRECT_DEBIT = "direct_debit_monthly"
+    }
+}
--- src/main/kotlin/com/damianryan/octopus/OctopusProperties.kt
+++ src/main/kotlin/com/damianryan/octopus/OctopusProperties.kt
@@ -9,17 +9,13 @@ import org.springframework.boot.context.properties.bind.ConstructorBinding
  *
  * @property accountNumber account number
  * @property apiKey API key
- * @property accountsUrl Octopus API accounts endpoint
- * @property electricityMpanUrl Octopus API electricity MPAN endpoint
- * @property electricityConsumptionUrl Octopus API electricity consumption endpoint
- * @property electricityStandingChargesUrl Octopus API electricity standing charges endpoint
- * @property electricityConsumptionUrl Octopus API electricity standard unit rates endpoint
- * @property tariffType tariff type
- * @property fixedRateProductCode fixed rate product code
- * @property goProductCode go product code
- * @property gasConsumptionUrl Octopus API gas consumption endpoint
+ * @property arrangementDate date when arrangement was made with Octopus
+ * @property periodFrom date when switch to Octopus actually started
+ * @property baseUrl Octopus API base URL
+ * @property electricityProductCode electricity product code
+ * @property electricityRegion electricity region (also known as distribution area, DNO region, or GSP group)
  */
-@ConfigurationProperties(prefix = "octopus")
+@ConfigurationProperties("octopus")
 data class OctopusProperties
 @ConstructorBinding
 constructor(
@@ -27,13 +23,9 @@ constructor(
     val apiKey: String,
     val arrangementDate: ZonedDateTime,
     val periodFrom: ZonedDateTime,
-    val accountsUrl: String? = null,
-    val electricityMpanUrl: String? = null,
-    val electricityConsumptionUrl: String? = null,
-    val electricityStandingChargesUrl: String? = null,
-    val electricityStandardUnitRatesUrl: String? = null,
-    val tariffType: String? = null,
-    val fixedRateProductCode: String? = null,
-    val goProductCode: String? = null,
-    val gasConsumptionUrl: String? = null,
-)
+    val baseUrl: String,
+    val electricityProductCode: String,
+    val electricityRegion: String
+) {
+    val accountsUrl = "/accounts/$accountNumber"
+}
