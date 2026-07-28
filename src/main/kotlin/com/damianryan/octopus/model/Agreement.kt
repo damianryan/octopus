f759d7afbbdf98b615caf11787f22fdd8b95372f
damianryan <github@damianryan.com>
2026-07-28 11:26:07 +0100

    Updated Agreement to permit extraction of information about product code, region, fuel & register types.

--- src/main/kotlin/com/damianryan/octopus/model/Agreement.kt
+++ src/main/kotlin/com/damianryan/octopus/model/Agreement.kt
@@ -4,10 +4,22 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.Instant
 
 data class Agreement(
-    @JsonProperty("tariff_code") val tariffCode: String? = null,
-    @JsonProperty("valid_from") val validFrom: Instant? = null,
+    @JsonProperty("tariff_code") val tariffCode: String,
+    @JsonProperty("valid_from") val validFrom: Instant,
     @JsonProperty("valid_to") val validTo: Instant? = null,
 ) : Comparable<Agreement> {
     override fun compareTo(other: Agreement): Int =
         compareValuesBy(other, this, Agreement::validFrom, Agreement::validTo)
+
+    val electricityRegion: String
+        get() = "_${tariffCode.substringAfterLast("-")}"
+
+    val fuelType: String
+        get() = tariffCode.substring(0, 1)
+
+    val registerType: String
+        get() = tariffCode.substring(2, 4)
+
+    val productCode: String
+        get() = tariffCode.substringBeforeLast("-").substring(5)
 }
---
+++ src/test/kotlin/com/damianryan/octopus/model/AgreementTest.kt
@@ -0,0 +1,68 @@
+package com.damianryan.octopus.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.time.Instant
+
+class AgreementTest {
+
+    @ParameterizedTest
+    @MethodSource("electricityAgreements")
+    fun `electricity agreement has the expected fuel type`(tariffCode: String) {
+        assertThat(Agreement(tariffCode, VALID_FROM).fuelType).isEqualTo("E")
+    }
+
+    @ParameterizedTest
+    @MethodSource("gasAgreements")
+    fun `gas agreement has the expected fuel type`(tariffCode: String) {
+        assertThat(Agreement(tariffCode, VALID_FROM).fuelType).isEqualTo("G")
+    }
+
+    @ParameterizedTest
+    @MethodSource("electricityAgreements")
+    fun `electricity agreement has the expected register type`(tariffCode: String) {
+        assertThat(Agreement(tariffCode, VALID_FROM).registerType).isEqualTo("1R")
+    }
+
+    @ParameterizedTest
+    @MethodSource("electricityAgreements")
+    fun `electricity agreement has the expected electricity region`(tariffCode: String) {
+        assertThat(Agreement(tariffCode, VALID_FROM).electricityRegion).isEqualTo("_A")
+    }
+
+    @ParameterizedTest
+    @MethodSource("products")
+    fun `agreement has the expected product code`(tariffCode: String, expectedProductCode: String) {
+        assertThat(Agreement(tariffCode, VALID_FROM).productCode).isEqualTo(expectedProductCode)
+    }
+
+    companion object {
+        val VALID_FROM: Instant = Instant.now()
+
+        @JvmStatic
+        fun electricityAgreements(): List<String> = listOf(
+            "E-1R-GO-FIX-12M-26-06-30-A",
+            "E-1R-VAR-22-11-01-A",
+            "E-1R-GO-VAR-22-10-14-A",
+            "E-1R-GO-22-03-29-A",
+            "E-1R-GO-21-05-13-A",
+            "E-1R-OE-FIX-24M-21-05-29-A"
+        )
+
+        @JvmStatic
+        fun products() = listOf(
+            Arguments.of("E-1R-GO-FIX-12M-26-06-30-A", "GO-FIX-12M-26-06-30"),
+            Arguments.of("E-1R-OE-FIX-24M-21-05-29-A", "OE-FIX-24M-21-05-29"),
+            Arguments.of("G-1R-OE-LOYAL-FIX-16M-25-02-12-A", "OE-LOYAL-FIX-16M-25-02-12")
+        )
+
+        @JvmStatic
+        fun gasAgreements(): List<String> = listOf(
+            "G-1R-VAR-22-11-01-A",
+            "G-1R-OE-LOYAL-FIX-16M-25-02-12-A",
+            "G-1R-OE-FIX-24M-21-05-29-A"
+        )
+    }
+}
