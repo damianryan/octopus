0e20296665abc19b1d47fa07a440694c5af000ad
damianryan <github@damianryan.com>
2026-09-05 16:41:44 +0100

    Added documentation to MdcPropagatingTaskScope and StructuredConcurrency.kt.
    Added @DefaultValue annotations to properties of RestClientLoggingProperties.
    
    Log whether a header is a request or response header for each header.
    
    Return the gas & electricity consumption readings from the task scope in OctopusApplication.

--- src/main/kotlin/com/damianryan/octopus/concurrency/MdcPropagatingTaskScope.kt
+++ src/main/kotlin/com/damianryan/octopus/concurrency/MdcPropagatingTaskScope.kt
@@ -5,7 +5,10 @@ import java.util.concurrent.StructuredTaskScope
 import org.slf4j.MDC
 
 /**
- * A wrapper around [StructuredTaskScope] that propagates the MDC context to forked tasks.
+ * A wrapper around [StructuredTaskScope] that propagates the MDC context to forked tasks. Since [AutoCloseable] is an
+ * interface, the wrapper can delegate the `close()` method to the underlying [StructuredTaskScope] instance, allowing
+ * it to be used in a `use` block (the Kotlin equivalent of Java's try-with-resources) to ensure that the scope is
+ * properly closed after use.
  *
  * @property scope underlying [StructuredTaskScope] that manages the tasks.
  * @param T result type of the tasks managed by this scope
@@ -14,9 +17,13 @@ import org.slf4j.MDC
 class MdcPropagatingTaskScope<T, R>(private val scope: StructuredTaskScope<T, R>) : AutoCloseable by scope {
 
     /**
-     * Fork a new task in this scope, propagating the MDC to it.
+     * Fork a new task in this scope, propagating the MDC to it. Accepts a task that returns a result of type [A],
+     * which must be a subtype of [T]. The forked task will run in the same MDC context as the caller. Returns a
+     * [StructuredTaskScope.Subtask] representing the forked task, allowing you to keep the more specific result type
+     * [A] while reusing the scope's [T].
      *
      * @param task the task to run
+     * @param A the type of the result returned by the task, which must be a subtype of [T]
      * @return a [StructuredTaskScope.Subtask] representing the forked task
      */
     fun <A : T> fork(task: () -> A): StructuredTaskScope.Subtask<A> {
--- src/main/kotlin/com/damianryan/octopus/concurrency/StructuredConcurrency.kt
+++ src/main/kotlin/com/damianryan/octopus/concurrency/StructuredConcurrency.kt
@@ -1,14 +1,32 @@
 package com.damianryan.octopus.concurrency
 
 import java.util.concurrent.StructuredTaskScope
+import java.util.concurrent.StructuredTaskScope.Joiner
 
 /**
- * Create a new [MdcPropagatingTaskScope] and execute the given block within it.
+ * Create a new [MdcPropagatingTaskScope] that can be used to fork subtasks that return results of type [T] and join
+ * them to produce a result of type [R]. The scope's `join()` method waits for all tasks to succeed or any subtask to
+ * fail. The `join()` method returns `null` if all subtasks succeed, or throws an exception if any subtask fails.
+ *
+ * Example usage:
+ * ```kotlin
+ * val productCodes = awaitAllSuccessfulOrThrow {
+ *     val electricityAgreements = fork { octopus.electricityAgreements() }
+ *     val gasAgreements = fork { octopus.gasAgreements() }
+ *     join()
+ *     val codes = electricityAgreements.get().map { it.productCode }.toMutableSet()
+ *     codes.addAll(gasAgreements.get().map { it.productCode })
+ *     codes.toSet()
+ * }
+ * ```
  *
  * @param block the block of code to execute within the task scope
  * @param T result type of the tasks managed by this scope
  * @param R the type of the result returned by joining the scope
+ * @throws StructuredTaskScope.FailedException if any subtask fails, with the first encountered exception as the cause
  * @return the result of joining the scope.
  */
 inline fun <T, R> awaitAllSuccessfulOrThrow(block: MdcPropagatingTaskScope<T, Void>.() -> R): R =
-    MdcPropagatingTaskScope(StructuredTaskScope.open<T>()).use { it.block() }
+    MdcPropagatingTaskScope(StructuredTaskScope.open(Joiner.awaitAllSuccessfulOrThrow<T>())).use { it.block() }
+
+
